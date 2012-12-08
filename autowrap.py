c3472e5b3a945a612fc885449d73ef35acb85df4
Randy Lai <shingx1@gmail.com>
2012-12-08 13:19:14 -0800

    simiplify the code

--- autowrap.py
+++ autowrap.py
@@ -1,35 +1,29 @@
 import sublime, sublime_plugin
 
 class AutoWrapListener(sublime_plugin.EventListener):
-    prev_sel = 0
-
-    def on_load(self, view):
-        pass
-
-    def on_selection_modified(self, view):
-        if view.is_scratch() or view.settings().get('is_widget'): return
-        if not view.settings().get('auto_wrap', False): return
-        self.prev_sel = view.sel()[0].end()
+    saved_sel = 0
 
     def on_modified(self, view):
         if view.is_scratch() or view.settings().get('is_widget'): return
         if not view.settings().get('auto_wrap', False): return
-        rulers = view.settings().get('rulers')
-        if not rulers: rulers = [80]
         sel = view.sel()
         if len(sel)>1 or sel[0].begin()!=sel[0].end(): return
+        rulers = view.settings().get('rulers')
+        if not rulers: rulers = [80]
         pt = sel[0].end()
-        if pt<=self.prev_sel or pt-self.prev_sel>1 or view.rowcol(pt)[1]<=rulers[0] \
+        if pt<=self.saved_sel or pt-self.saved_sel>1 or view.rowcol(pt)[1]<=rulers[0] \
             or view.substr(pt-1)==" " or view.line(pt).end() != pt:
-            return
+            activate = False
+        else: activate = True
+        self.saved_sel = sel[0].end()
+        if not activate: return
 
         # to obtain the insert point
-        saved_sel = sel[0].end()
         view.run_command("move", {"by": "stops", "word_begin": True, "empty_line": True, "separators": "", "forward": False})
         insertpt = view.sel()[0].end()
         view.sel().clear()
-        view.show(saved_sel)
-        view.sel().add(saved_sel)
+        view.show(self.saved_sel)
+        view.sel().add(self.saved_sel)
 
         # insert enter
         edit_insert = view.begin_edit()
